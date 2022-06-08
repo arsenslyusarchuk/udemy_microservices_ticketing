b1f9ca84fe3f544ba207abb9866cbfac5b7caeb2
Arsen Slyusarchuk <arsen.slyusarchuk@WS-LV-CP4491.eleks-software.local>
2022-06-08 11:52:33 +0300

    Fix tests orders

--- orders/src/routes/index.ts
+++ orders/src/routes/index.ts
@@ -5,11 +5,9 @@ import { Order } from '../models/order';
 const router = express.Router();
 
 router.get('/api/orders', requireAuth, async (req: Request, res: Response) => {
-  // const orders = await Order.find({
-  //   userId: req.currentUser!.id,
-  // }).populate('ticket');
-
-  const orders = await Order.find().populate('ticket');
+  const orders = await Order.find({
+    userId: req.currentUser!.id,
+  }).populate('ticket');
 
   res.send(orders);
 });
